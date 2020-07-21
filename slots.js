0bf4f0c7cae2c0e905ecd77210fe8a3e443aaa88
Katherine <katherineprintz@gmail.com>
2020-07-21 16:01:13 -0600

    Using template approach to inject number of reels

--- slots.js
+++ slots.js
@@ -8,10 +8,36 @@ class SlotMachine {
         this.target = document.getElementById('spin-message');
         this.winMessage = settings.winMessage;
         this.loseMessage = settings.loseMessage;
-        this.reelPosibilities = settings.reelPosibilities;
+        this.reelPossibilities = settings.reelPossibilities;
         this.reelHeight = settings.reelHeight;
         this.spinsLoopTilStop = settings.spinsLoopTilStop;
+        this.reelRow = settings.reelRow;
+        this.reelCol = settings.reelCol;
+        this.reelTemplate = document.getElementById('reel-1');
+
+        this.createSlotsHtml();
     };
+
+    createSlotsHtml() {
+        let reelId = 1;
+        for (let i = 0; i < this.reelRow; i++) {
+
+
+
+            
+            for (let e = 0; e < this.reelCol; e++) {
+                let newReel = this.reelTemplate.cloneNode(true);
+                newReel.id = 'reel-' + reelId;
+                reelId++;
+                this.reelTemplate.after(newReel);
+            }
+
+
+
+
+        }
+        this.reelTemplate.remove();
+    }
 
     handleUserClick() {
         // reset the spin count and start spinning reel
@@ -25,7 +51,7 @@ class SlotMachine {
         // spins the reel
         for (let reel of this.reelHolders) {
             reel.style.transition = 'all 400ms linear 0s';
-            reel.style.transform = 'translateY(-'+ this.reelPosibilities * this.reelHeight +'px)';
+            reel.style.transform = 'translateY(-'+ this.reelPossibilities * this.reelHeight +'px)';
         }
         // determine winner and stop spin
         if (this.spin > this.spinsLoopTilStop) {
@@ -69,15 +95,16 @@ class SlotMachine {
     }
 
     getRandNumber() {
-        return Math.floor(Math.random() * this.reelPosibilities + 1);
+        return Math.floor(Math.random() * this.reelPossibilities + 1);
     }
 }
 
 let slotMachine = new SlotMachine({
-    numberOfReels: 3,
+    reelRow: 3,
+    reelCol: 3,
     winMessage: 'Winner!',
     loseMessage: 'No goood',
-    reelPosibilities: 4,
+    reelPossibilities: 4,
     reelHeight: 60,
     spinsLoopTilStop: 3,
     spinMilliseconds: 400
